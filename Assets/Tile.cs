f38ecfffb69726a5b5cb7bcfb233df32ed66d362
crowsCaledfwlch <122137319+crowsCaledfwlch@users.noreply.github.com>
2023-08-30 13:39:28 -0400

    Grid updates

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Tile above, upL, upR, below, downL, downR;
+    private bool set;
+    private bool centre;
+    private int type;
+    private SpriteRenderer renderer;
+
     void Start()
     {
-        
+        renderer = GetComponent<SpriteRenderer>();
+        set = false;
+        centre = false;
+        type = 0;
+    }
+    // Start is called before the first frame update
+    public void setAbove(Tile tile)
+    {
+        above = tile;
+    }
+    public void setUpL(Tile tile)
+    {
+        upL = tile;
+    }
+    public void setUpR(Tile tile)
+    {
+        upR = tile;
+    }
+    public void setBelow(Tile tile)
+    {
+        below = tile;
+    }
+    public void setDownL(Tile tile)
+    {
+        downL = tile;
+    }
+    public void setDownR(Tile tile)
+    {
+        downR = tile;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseOver()
     {
-        
+        if (!set)
+        {
+            switch (type)
+            {
+                case 0:
+                    renderer.color = Color.blue;
+                    break;
+            }
+        }
+    }
+    void OnMouseExit()
+    {
+        if (!set)
+        {
+            renderer.color = Color.white;
+        }
     }
 }
