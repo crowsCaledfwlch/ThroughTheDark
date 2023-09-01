c9158e4af48a62c85496963fd0253b483e02067b
crowsCaledfwlch <122137319+crowsCaledfwlch@users.noreply.github.com>
2023-08-31 23:11:46 -0400

    tile overhaul

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -3,64 +3,292 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+namespace TTD
 {
-    private Tile above, upL, upR, below, downL, downR;
-    private bool set;
-    private bool centre;
-    private int type;
-    private SpriteRenderer renderer;
 
-    void Start()
+    public class Tile : MonoBehaviour
     {
-        renderer = GetComponent<SpriteRenderer>();
-        set = false;
-        centre = false;
-        type = 0;
-    }
-    // Start is called before the first frame update
-    public void setAbove(Tile tile)
-    {
-        above = tile;
-    }
-    public void setUpL(Tile tile)
-    {
-        upL = tile;
-    }
-    public void setUpR(Tile tile)
-    {
-        upR = tile;
-    }
-    public void setBelow(Tile tile)
-    {
-        below = tile;
-    }
-    public void setDownL(Tile tile)
-    {
-        downL = tile;
-    }
-    public void setDownR(Tile tile)
-    {
-        downR = tile;
-    }
+        public Tile above, upR, downR, below, downL, upL; // 1 2 3 4 5 6. 1->3->6->4->5->2->1  
+        bool set;
 
-    void OnMouseOver()
-    {
-        if (!set)
+        [Range(0, 3)] // 0 plain, 1 treasure, 2 monster, 3 pitfall
+        private int type;
+
+        [Range(0, 2)] // 00 line, 01 blob, 02 corner, 10-2 2 tiles, 20 M, 21 Y, 22 line, 30-2 big C
+        private int shape;
+
+        [Range(0, 5)]
+        int rotation;
+
+        Dictionary<(int, int), (int, int, int, int)> tilesPositions;
+        private SpriteRenderer renderer;
+
+        void Start()
         {
-            switch (type)
+            renderer = GetComponent<SpriteRenderer>();
+            set = false;
+            //centre = false;
+            type = 0;
+            shape = 0;
+            rotation = 0;
+            tilesPositions = new Dictionary<(int, int), (int, int, int, int)>()
             {
-                case 0:
-                    renderer.color = Color.blue;
-                    break;
+                {(0,0), (1,1,0,0)},{(0,1), (1,3,0,0)},{(0,6), (1,2,0,0)},
+                {(1,0), (1,0,0,0)},
+                {(2,0), (6,1,2,0)},{(2,1), (1,1,3,0)},{(2,2), (1,1,1,0)},
+                {(3,0), (6,1,1,2)}
+            };
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown("r"))
+            {
+                OnMouseExit();
+                rotation++;
+                if (rotation > 5)
+                {
+                    rotation = 0;
+                }
             }
         }
-    }
-    void OnMouseExit()
-    {
-        if (!set)
+
+        SpriteRenderer GetSpriteRenderer() => renderer;
+
+        public void setAbove(Tile tile)
         {
-            renderer.color = Color.white;
+            above = tile;
+        }
+        public void setUpL(Tile tile)
+        {
+            upL = tile;
+        }
+        public void setUpR(Tile tile)
+        {
+            upR = tile;
+        }
+        public void setBelow(Tile tile)
+        {
+            below = tile;
+        }
+        public void setDownL(Tile tile)
+        {
+            downL = tile;
+        }
+        public void setDownR(Tile tile)
+        {
+            downR = tile;
+        }
+
+        public void setType(int type)
+        {
+            this.type = type;
+        }
+
+        public void setShape(int shape)
+        {
+            this.shape = shape;
+        }
+
+        bool checkTiles(int[] ints, Tile tile, int rotation)
+        {
+            if (ints.Length == 0 || tile == null)
+            {
+                return !tile.set;
+            }
+            else
+            {
+                int[] temp = new int[ints.Length - 1];
+                for (int i = 1; i < ints.Length; i++)
+                {
+                    temp[i - 1] = ints[i];
+                }
+                Tile tempTile = null;
+
+                if (ints[0] == 0) return !tile.set;
+                switch ((ints[0] + rotation) % 6)
+                {
+                    case 1:
+                        if (tile.above == null) return false;
+                        tempTile = tile.above;
+                        break;
+                    case 2:
+                        if (tile.upR == null) return false;
+                        tempTile = tile.upR;
+                        break;
+                    case 3:
+                        if (tile.downR == null) return false;
+                        tempTile = tile.downR;
+                        break;
+                    case 4:
+                        if (tile.below == null) return false;
+                        tempTile = tile.below;
+                        break;
+                    case 5:
+                        if (tile.downL == null) return false;
+                        tempTile = tile.downL;
+                        break;
+                    case 0:
+                        if (tile.upL == null) return false;
+                        tempTile = tile.upL;
+                        break;
+                }
+                return checkTiles(temp, tempTile, rotation) && !tile.set;
+            }
+        }
+
+        void colorTiles(int[] ints, Tile tile, int type, int rotation)
+        {
+            if (ints.Length == 0)
+            {
+                switch (type)
+                {
+                    case -1:
+                        tile.GetSpriteRenderer().color = Color.white;
+                        break;
+                    case 0:
+                        tile.GetSpriteRenderer().color = Color.yellow;
+                        break;
+                    case 1:
+                        tile.GetSpriteRenderer().color = Color.blue;
+                        break;
+                    case 2:
+                        tile.GetSpriteRenderer().color = Color.red;
+                        break;
+                    case 3:
+                        tile.GetSpriteRenderer().color = Color.black;
+                        break;
+                }
+            }
+            else
+            {
+
+                int[] temp = new int[ints.Length - 1];
+                for (int i = 1; i < ints.Length; i++)
+                {
+                    temp[i - 1] = ints[i];
+                }
+                Tile tempTile = null;
+                switch (type)
+                {
+                    case -1:
+                        tile.GetSpriteRenderer().color = Color.white;
+                        break;
+                    case 0:
+                        tile.GetSpriteRenderer().color = Color.yellow;
+                        break;
+                    case 1:
+                        tile.GetSpriteRenderer().color = Color.blue;
+                        break;
+                    case 2:
+                        tile.GetSpriteRenderer().color = Color.red;
+                        break;
+                    case 3:
+                        tile.GetSpriteRenderer().color = Color.black;
+                        break;
+                }
+
+                if (ints[0] == 0) return;
+                switch ((ints[0] + rotation) % 6)
+                {
+                    case 1:
+                        tempTile = tile.above;
+                        break;
+                    case 2:
+                        tempTile = tile.upR;
+                        break;
+                    case 3:
+                        tempTile = tile.downR;
+                        break;
+                    case 4:
+                        tempTile = tile.below;
+                        break;
+                    case 5:
+                        tempTile = tile.downL;
+                        break;
+                    case 0:
+                        tempTile = tile.upL;
+                        break;
+                }
+
+                colorTiles(temp, tempTile, type, rotation);
+            }
+        }
+
+        void OnMouseOver()
+        {
+            if (!set)
+            {
+                (int, int, int, int) nextStep = tilesPositions[(type, shape)];
+                if (checkTiles(new int[] { nextStep.Item1, nextStep.Item2, nextStep.Item3, nextStep.Item4 }, this, rotation))
+                {
+                    colorTiles(new int[] { nextStep.Item1, nextStep.Item2, nextStep.Item3, nextStep.Item4 }, this, type, rotation);
+                }
+            }
+        }
+        void OnMouseExit()
+        {
+            if (!set)
+            {
+                (int, int, int, int) nextStep = tilesPositions[(type, shape)];
+                if (checkTiles(new int[] { nextStep.Item1, nextStep.Item2, nextStep.Item3, nextStep.Item4 }, this, rotation))
+                {
+                    colorTiles(new int[] { nextStep.Item1, nextStep.Item2, nextStep.Item3, nextStep.Item4 }, this, -1, rotation);
+                }
+            }
+        }
+        void OnMouseDown()
+        {
+            if (!set)
+            {
+                (int, int, int, int) nextStep = tilesPositions[(type, shape)];
+                if (checkTiles(new int[] { nextStep.Item1, nextStep.Item2, nextStep.Item3, nextStep.Item4 }, this, rotation))
+                {
+                    setTrue(new int[] { nextStep.Item1, nextStep.Item2, nextStep.Item3, nextStep.Item4 }, this, type, rotation);
+                }
+            }
+        }
+
+        void setTrue(int[] ints, Tile tile, int type, int rotation)
+        {
+
+            if (ints.Length == 0)
+            {
+                tile.set = true;
+            }
+            else
+            {
+                tile.set = true;
+                int[] temp = new int[ints.Length - 1];
+                for (int i = 1; i < ints.Length; i++)
+                {
+                    temp[i - 1] = ints[i];
+                }
+                Tile tempTile = null;
+                if (ints[0] == 0) return;
+                switch ((ints[0] + rotation) % 6)
+                {
+                    case 1:
+                        tempTile = tile.above;
+                        break;
+                    case 2:
+                        tempTile = tile.upR;
+                        break;
+                    case 3:
+                        tempTile = tile.downR;
+                        break;
+                    case 4:
+                        tempTile = tile.below;
+                        break;
+                    case 5:
+                        tempTile = tile.downL;
+                        break;
+                    case 0:
+                        tempTile = tile.upL;
+                        break;
+                }
+                setTrue(temp, tempTile, type, rotation);
+            }
         }
     }
 }
--- Assets/tilegrid.cs
+++ Assets/tilegrid.cs
@@ -2,50 +2,114 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tilegrid : MonoBehaviour
+namespace TTD
 {
-    [SerializeField] private Tile _tilePrefab;
-    [SerializeField] private int _width;
-    [SerializeField] private int _height;
-
-
-    void Start()
+    public class tilegrid : MonoBehaviour
     {
-        GenerateGrid();
-    }
+        [SerializeField] private Tile _tilePrefab;
+        [SerializeField] private int _width;
+        [SerializeField] private int _height;
+        Dictionary<(int, int), Tile> tiles;
 
-    void GenerateGrid()
-    {
-        // very complicated math to make the grid and center it
-        for (int x = (-1 * _width) + 1; x < _width + 2; x += 2)
+
+        void Start()
         {
-            int i = 0;
-            for (int y = (int)(-0.5f * _height) + (int)(0.25f * _height); y < (int)(0.75f * _height) - 1; y += 1)
+            GenerateGrid();
+        }
+
+        void GenerateGrid()
+        {
+            tiles = new Dictionary<(int, int), Tile>();
+            // very complicated math to make the grid and center it
+            for (int x = (-1 * _width) + 1; x < _width + 2; x += 2)
             {
-                Tile spawnedTile;
-                if (y % 2 == 0)
+                int i = 0;
+                for (int y = (int)(-0.5f * _height) + (int)(0.25f * _height); y < (int)(0.75f * _height) - 1; y += 1)
                 {
-                    if (x < _width)
+                    Tile spawnedTile;
+                    if (y % 2 == 0)
                     {
-                        spawnedTile = Instantiate(_tilePrefab, new Vector3(x - .5f, y - i / 2, -1), Quaternion.identity);
+                        if (x < _width)
+                        {
+                            spawnedTile = Instantiate(_tilePrefab, new Vector3(x - .5f, y - i / 2, -1), Quaternion.identity);
+                            spawnedTile.name = $"Tile {x} {y}";
+                            spawnedTile.transform.parent = gameObject.transform;
+                            tiles[(x, y)] = spawnedTile;
+                        }
+                    }
+                    else
+                    {
+                        spawnedTile = Instantiate(_tilePrefab, new Vector3(x - 1.5f, y - i / 2 - 0.5f, -1), Quaternion.identity);
                         spawnedTile.name = $"Tile {x} {y}";
                         spawnedTile.transform.parent = gameObject.transform;
+                        tiles[(x, y)] = spawnedTile;
+                    }
+                    i++;
+                }
+            }
+            foreach (KeyValuePair<(int x, int y), Tile> tkvp in tiles)
+            {
+                if (tkvp.Key.y % 2 == 0)
+                {
+                    if (tiles.ContainsKey((tkvp.Key.x, tkvp.Key.y + 2)))
+                    {
+                        tkvp.Value.setAbove(tiles[(tkvp.Key.x, tkvp.Key.y + 2)]);
+                    }
+                    if (tiles.ContainsKey((tkvp.Key.x, tkvp.Key.y - 2)))
+                    {
+                        tkvp.Value.setBelow(tiles[(tkvp.Key.x, tkvp.Key.y - 2)]);
+                    }
+                    if (tiles.ContainsKey((tkvp.Key.x, tkvp.Key.y + 1)))
+                    {
+                        tkvp.Value.setUpL(tiles[(tkvp.Key.x, tkvp.Key.y + 1)]);
+                    }
+                    if (tiles.ContainsKey((tkvp.Key.x + 2, tkvp.Key.y + 1)))
+                    {
+                        tkvp.Value.setUpR(tiles[(tkvp.Key.x + 2, tkvp.Key.y + 1)]);
+                    }
+                    if (tiles.ContainsKey((tkvp.Key.x, tkvp.Key.y - 1)))
+                    {
+                        tkvp.Value.setDownL(tiles[(tkvp.Key.x, tkvp.Key.y - 1)]);
+                    }
+                    if (tiles.ContainsKey((tkvp.Key.x + 2, tkvp.Key.y - 1)))
+                    {
+                        tkvp.Value.setDownR(tiles[(tkvp.Key.x + 2, tkvp.Key.y - 1)]);
                     }
                 }
                 else
                 {
-                    spawnedTile = Instantiate(_tilePrefab, new Vector3(x - 1.5f, y - i / 2 - 0.5f, -1), Quaternion.identity);
-                    spawnedTile.name = $"Tile {x} {y}";
-                    spawnedTile.transform.parent = gameObject.transform;
+                    if (tiles.ContainsKey((tkvp.Key.x, tkvp.Key.y + 2)))
+                    {
+                        tkvp.Value.setAbove(tiles[(tkvp.Key.x, tkvp.Key.y + 2)]);
+                    }
+                    if (tiles.ContainsKey((tkvp.Key.x, tkvp.Key.y - 2)))
+                    {
+                        tkvp.Value.setBelow(tiles[(tkvp.Key.x, tkvp.Key.y - 2)]);
+                    }
+                    if (tiles.ContainsKey((tkvp.Key.x - 2, tkvp.Key.y + 1)))
+                    {
+                        tkvp.Value.setUpL(tiles[(tkvp.Key.x - 2, tkvp.Key.y + 1)]);
+                    }
+                    if (tiles.ContainsKey((tkvp.Key.x, tkvp.Key.y + 1)))
+                    {
+                        tkvp.Value.setUpR(tiles[(tkvp.Key.x, tkvp.Key.y + 1)]);
+                    }
+                    if (tiles.ContainsKey((tkvp.Key.x - 2, tkvp.Key.y - 1)))
+                    {
+                        tkvp.Value.setDownL(tiles[(tkvp.Key.x - 2, tkvp.Key.y - 1)]);
+                    }
+                    if (tiles.ContainsKey((tkvp.Key.x, tkvp.Key.y - 1)))
+                    {
+                        tkvp.Value.setDownR(tiles[(tkvp.Key.x, tkvp.Key.y - 1)]);
+                    }
                 }
-                i++;
             }
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
 
+        }
     }
 }
